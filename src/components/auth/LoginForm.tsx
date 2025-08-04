8f422fa89d87774dfee326aeeffedd3187078e44
Kunzeu <kunjohn24@gmail.com>
2025-08-03 23:23:15 -0500

    test

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -195,7 +195,7 @@ export default function LoginForm() {
         {/* Links */}
         <div className="mt-6 text-center">
           <p className="text-gray-400 text-sm">
-            Don't have an account?{' '}
+            Don&apos;t have an account?{' '}
             <a href="/register" className="text-blue-400 hover:text-blue-300 font-medium">
               Sign up here
             </a>
