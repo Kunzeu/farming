28b0a6e316d244192f06e45eb67fd3549cb2b6a5
Kunzeu <kunjohn24@gmail.com>
2025-10-30 19:42:18 -0500

    prueba

--- src/app/api/auth/patreon/verify-member/route.ts
+++ src/app/api/auth/patreon/verify-member/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyMemberPaidByPatreonId } from '@/lib/server/patreon-creator';
 
-export const runtime = 'edge';;
+export const runtime = 'nodejs';
 
 export async function GET(request: NextRequest) {
   try {
